35ef4fc529138204aeecf3e0f7219dab833f5e65
Matthias Bussonnier <bussonniermatthias@gmail.com>
2018-10-22 10:30:48 -0700

    nl

--- jupyter_client/slurmkernelmanager.py
+++ jupyter_client/slurmkernelmanager.py
@@ -56,7 +56,7 @@ class SlurmKernelManager(IOLoopKernelManager):
             f.write("#SBATCH --ntasks=1\n")
             f.write("#SBATCH --node=1\n")
             f.write('JUPYTER_RUNTIME_DIR="$HOME/xd/"\n')
-            f.write('echo Running:' + ' '.join(kernel_cmd))
+            f.write('echo Running:' + ' '.join(kernel_cmd)+'\n')
             f.write(' '.join(kernel_cmd))
         r = run("sbatch ssubmit.sh".split(" "), stdout=PIPE, stderr=PIPE)
         jobid = r.stdout.strip().split(b" ")[-1].decode()
